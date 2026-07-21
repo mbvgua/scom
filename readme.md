bce5f118c5afb30c82e72e88118e1d0e60aeee96
mbvgua <mbvgua+dev@gmail.com>
2026-07-21 14:33:48 +0300

    feat: intial commit
    CHANGES:
    - create the base project, very plain file setting, only working with
      plain html, css and javascript. making it minimal as possible to allow
      for buildingmore features without much hiccup. would also like to move
      to using a backend framework, hence templates being displayed in plain
      html and css is soooo conveneient.

---
+++ readme.md
@@ -0,0 +1,10 @@
+# scom
+
+main website for scom children's home
+
+## roadmap
+
+- [ ] build basic website in html, css and js
+- [ ] add framework afterwards, maybe backend based(fastAPI)
+- [ ] containerize and deploy the application
+- [ ] work around getting a domain
---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("mainNav");
+
+    // Listen for scroll events on the window
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            // User scrolled down -> add solid background & shrink
+            navbar.classList.add("scrolled");
+        } else {
+            // User is at the top -> make transparent again
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
