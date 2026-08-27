ac303e30cf0c63d1eb2276cf6dee21d64adb7cc4
mbvgua <mbvgua+dev@gmail.com>
2026-08-27 12:05:21 +0300

    feat: add admin image

--- app/data.py
+++ app/data.py
@@ -150,7 +150,7 @@ TEAM = [
     {
         "id": 1,
         "name": "Mr. Meshack Okoth",
-        "title": "Director",
+        "title": "Director/Founder",
         "image": {
             "src": "images/team/Meshack-Okoth.webp",
             "alt": "The Director",
@@ -183,15 +183,15 @@ TEAM = [
             "alt": "The Admin",
         },
     },
-    {
-        "id": 5,
-        "name": "Mr. Don",
-        "title": "International Coordinator",
-        "image": {
-            "src": "images/team/Don.webp",
-            "alt": "The International Coordinator",
-        },
-    },
+    # {
+    #     "id": 5,
+    #     "name": "Mr. Don",
+    #     "title": "International Coordinator",
+    #     "image": {
+    #         "src": "images/team/Don.webp",
+    #         "alt": "The International Coordinator",
+    #     },
+    # },
 ]
 
 
